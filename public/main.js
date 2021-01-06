aed954d48b109b04e3dbe412157bc4279027b959
ho van la <hovanminh5@gmail.com>
2021-01-06 17:35:53 +0700

    finish fist

---
+++ public/main.js
@@ -0,0 +1,217 @@
+$(document).ready(function(){
+
+    function uuid() {
+        var temp_url = URL.createObjectURL(new Blob());
+        var uuid = temp_url.toString();
+        URL.revokeObjectURL(temp_url);
+        return uuid.substr(uuid.lastIndexOf('/') + 1); 
+      }
+    $('#Add').click(function(){
+        let apikey=uuid();
+        let idrow=apikey;
+       
+        let rowtb=`<tr id="${apikey}">
+        <td>
+            <span class="custom-checkbox">
+            <input type="checkbox" name="options[]" value="${apikey}">
+            <label for="checkbox1"></label>
+            </span>
+        </td>
+        <td class="param d-flex flex-row"><span>/</span><input name="param" type="url"></td>
+        <td class="name"><input name="name" type="text"></td>
+        <td class="urliframe"><input type="url" name="url"></td>
+        <td>
+            <a href="javascript:function() { return false; }" class="save" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="" data-original-title="Edit">save
+            </i></a>
+            <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="" data-original-title="Delete"></i></a>
+        </td>
+    </tr>
+   
+    `;
+     $("tbody").prepend(rowtb);
+     $("#"+idrow+" .name input").css('border','double');
+     $("#"+idrow+" .param input").css('border','double');
+     $("#"+idrow+" .urliframe input").css('border','double');
+     $("#"+idrow+" .param input").focus(); 
+    });
+    $('#addFilterr').click(function(){
+        let id=$("#ab").data("id");
+        let name=$("input[name='namef']").val();
+        let namebt=$(this).val();
+        let dt={
+            "name":name
+        };
+        if(namebt=="Add"){
+            $.ajax({
+                method: "POST",
+                url: "/insert",
+                data:dt
+            }).done(function( msg ) {
+                let rowtb=`<tr id="${msg.id}">
+                    <td>
+                        <span class="custom-checkbox">
+                        <input type="checkbox" name="options[]" value="${msg.id}">
+                        <label for="checkbox1"></label>
+                        </span>
+                    </td>
+                    <td>${msg.id}</td>
+                    <td class="name"><a href="/filter/${msg.id}"> ${name}</a></td>
+                    <td>
+                        <a href="#" class="editt" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="" data-original-title="Edit">&#xe3c9</i></a>
+                        <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="" data-original-title="Delete">&#xE872;</i></a>
+                    </td>
+                </tr>`;
+                 $("tbody").append(rowtb);
+                $('#addfilter').modal('hide');
+            });
+        }else{
+            $.ajax({
+                method: "PUT",
+                url: "/update/"+id,
+                data:dt
+            }).done(function( msg ) {
+                $("#"+id+" .name").text(name);
+                $('#addfilter').modal('hide');
+            });
+        }
+        
+    });
+    $('#tbfilter').on('click', '.update', function() {
+        $(this).removeClass("update").addClass("editt");
+        $(this).find("i").text("edit")
+        let idrow=$(this).closest('tr').attr('id');
+        $("#"+idrow+" .param input").attr("readonly", true); 
+        $("#"+idrow+" .param input").css('border','none');
+        $("#"+idrow+" .urliframe input").css('border','none');
+        $("#"+idrow+" .urliframe input").attr("readonly", true); 
+        $("#"+idrow+" .name input").attr("readonly", true); 
+        $("#"+idrow+" .name input").css('border','none');
+        let username=$(".username").text();
+        let name=$("#"+idrow+" .name input").val();
+        let param=$("#"+idrow+" .param input").val();
+        let urliframe=$("#"+idrow+" .urliframe input").val();
+        const nDate = new Date().toLocaleString('en-US', {
+            timeZone: 'Asia/Ho_Chi_Minh'
+          });
+        var myDate = Date.parse(nDate);
+        let dt={
+            "name":name,
+            "param":param,
+            "urliframe":urliframe,
+            "updateat":myDate,
+            "modifyby":username
+        };
+        $.ajax({
+            method: "put",
+            url: "/api/update/"+idrow,
+            data:dt
+            }).done(function( msg ) {
+                if(msg.status=="duplicate"){
+                    $("#"+msg.ifold.id+" .name input").val(msg.ifold.name);
+                    $("#"+msg.ifold.id+" .param input").val(msg.ifold.param);
+                    $("#"+msg.ifold.id+" .urliframe input").val(msg.ifold.urliframe);
+                    alert("Duplicate parameters!")
+                }
+            });
+
+    });
+    $('#tbfilter').on('click', '.save', function() {
+        $(this).find("i").text("edit")
+        $(this).removeClass("save").addClass("editt");
+        let idrow=$(this).closest('tr').attr('id');
+        $("#"+idrow+" .param input").attr("readonly", true); 
+        $("#"+idrow+" .param input").css('border','none');
+        $("#"+idrow+" .name input").css('border','none');
+        $("#"+idrow+" .name input").attr("readonly", true); 
+        $("#"+idrow+" .urliframe input").attr("readonly", true); 
+        $("#"+idrow+" .urliframe input").css('border','none');
+        let name=$("#"+idrow+" .name input").val();
+       let param=$("#"+idrow+" .param input").val();
+        let urliframe=$("#"+idrow+" .urliframe input").val();
+        let username=$(".username").text();
+        let todaysDate =new Date(); 
+        const nDate = todaysDate.toLocaleString('en-US', {
+            timeZone: 'Asia/Ho_Chi_Minh'
+          });
+        var myDate = Date.parse(nDate);
+        let dt={
+            "id":idrow,
+            "name":name,
+            "param":param,
+            "urliframe":urliframe,
+            "createdat":myDate,
+            "createby":username
+        };
+        $.ajax({
+            method: "post",
+            url: "/api/insert",
+            data:dt
+            }).done(function( msg ) {
+                if(msg.status=="duplicate"){
+                    $('#tbfilter tr#'+msg.id).remove();
+                    alert("Duplicate parameters!")
+                }
+                // $('#deleteEmployeeModal').modal('hide');
+            });
+    });
+    $('#tbfilter').on('click', '.editt', function() {
+       $(this).find("i").text("save")
+        $(this).removeClass("editt").addClass("update");
+        let idrow=$(this).closest('tr').attr('id');
+        $("#"+idrow+" .name input").css('border','double');
+        $("#"+idrow+" .name input").attr("readonly", false); 
+        $("#"+idrow+" .urliframe input").css('border','double');
+        $("#"+idrow+" .urliframe input").attr("readonly", false); 
+        $("#"+idrow+" .param input").css('border','double');
+        $("#"+idrow+" .param input").attr("readonly", false); 
+        $("#"+idrow+" .param input").focus(); 
+    });
+	$('#tbfilter').on('click', '.delete', function() {
+		$('.remove').attr("data-id","only");
+		var ID =$(this).closest('tr').attr('id');
+		$("#ab").data("id",ID)
+	
+    });
+    $('#DeleteAll').click(function(){
+		$('.remove').attr("data-id","all");
+    });
+    $('.remove').click(function(){
+        var chkb = $("input[name='options[]']:checked").map(function(){return $(this).val();}).get();
+        if($('.remove').attr("data-id")=="all"){
+			if(chkb.length>0){
+				for(l in chkb){
+                    // $('#tbfilter tr#'+chkb[l]).next().remove();
+					$('#tbfilter tr#'+chkb[l]).remove();
+					$.ajax({
+					method: "DELETE",
+					url: "/api/delete/"+chkb[l],
+					}).done(function( msg ) {
+						$('#deleteEmployeeModal').modal('hide');
+					});
+				}
+			}else{
+				alert("You have not selected any item!")
+			}
+		}else{
+            // $('#tbfilter tr#'+$("#ab").data("id")).next().remove();
+            $('#tbfilter tr#'+$("#ab").data("id")).remove();
+			$.ajax({
+				method: "DELETE",
+				url: "api/delete/"+$("#ab").data("id"),
+				}).done(function( msg ) {
+					$('#deleteEmployeeModal').modal('hide');
+				});
+		}
+    })
+    $('[data-toggle="tooltip"]').tooltip();
+    var checkbox = $('table tbody input[type="checkbox"]');
+	$("#selectAll").click(function(){
+		$('input:checkbox').not(this).prop('checked', this.checked);
+	});
+	
+	checkbox.click(function(){
+		if(!this.checked){
+			$("#selectAll").prop("checked", false);
+		}
+	});
+});
---
+++ server.js
@@ -0,0 +1,225 @@
+var PORT=process.env.PORT||8083;
+var express = require('express');
+var app = express();
+const path = require('path');
+const bcrypt = require('bcryptjs')
+const cookieParser = require('cookie-parser')
+var url = require('url');
+const jwt = require("jsonwebtoken");
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views')); 
+app.use(cookieParser())
+const low = require('lowdb');
+app.set('view engine', 'ejs');
+const FileSync = require('lowdb/adapters/FileSync');
+const { Console } = require('console');
+const adapter = new FileSync(__dirname+'/db.json');
+const db = low(adapter);
+db.defaults({ posts: [], user: []})
+  .write();
+function getalldt(){
+  return db.get('posts').value();
+}
+var dataall=getalldt();
+function getvl(k){
+  for(x in dataall){
+    if(dataall[x]['param']==k){
+      return dataall[x];
+    }
+  }
+}
+function getid(k){
+  for(x in dataall){
+    if(dataall[x]['id']==k){
+      return dataall[x];
+    }
+  }
+}
+function checkToken (req, res, next) {
+    const authcookie = req.cookies.authcookie
+    jwt.verify(authcookie,"iframe",(err,data)=>{
+    if(err){
+     res.redirect("login")
+    } 
+    else if(data.user){
+      req.user = data.user
+      next()
+    }
+})};
+app.get('/:pram*', function (req, res,next) {
+  let website = req.protocol + '://' + req.get('host') + req.originalUrl; 
+  let parse = url.parse(website, true);
+  let wp=parse.search;
+  let s3="";
+  if(wp){
+    s3=wp;
+  }
+      let dl=getvl(req.params.pram);
+      if(typeof dl ==="undefined"){
+        next();
+      }else{
+        let ifr=dl.urliframe;
+        let name=dl.name;
+        let s2=req.params[0];
+        res.render('iframe',{ifr,name,s2,s3});
+      }
+});
+
+
+app.get('/register', function(req, res) {
+    let error=[];
+    res.render('register',{error});
+});
+app.get('/password', function(req, res) {
+  let error=[];
+  res.render('password',{error});
+});
+app.post('/password', async function(req, res) {
+  let error=[];
+    let isPasswordMatch=false;
+   
+    let ifus=db.get('user').find({username: req.body["username"]}).value();
+    if(typeof ifus !="undefined"){
+      isPasswordMatch = await bcrypt.compare(req.body["oldpass"], ifus.password)
+    }else{
+      error.push('Username exists!');
+      rser()
+    }
+    if(!isPasswordMatch){
+      error.push('Wrong old password!')
+      rser()
+    }
+    if(req.body["newpass"]!=req.body["newpasss"]){
+      error.push("The new password doesn't match!");
+      rser()
+    }
+    function rser(){
+      res.render('password',{error});
+    }
+    let pws=await bcrypt.hash(req.body["newpass"], 8)
+    let username=req.body["username"];
+    if(error.length==0){
+      db.get('user')
+      .find({'username':username})
+      .assign({'password':pws})
+      .write();
+      res.cookie('authcookie',"token",{maxAge:0,httpOnly:true}) 
+      res.redirect("login")
+    }
+});
+app.post('/register', async function(req, res) {
+    let error=[];
+    let pws=await bcrypt.hash(req.body["psw"], 8)
+    let username=req.body["username"];
+
+    let ttus = db
+    .get('user')
+    .find({ username: username })
+    .value();
+    if(typeof ttus=="undefined"){
+      let user={
+        "username":username,
+        "password":pws
+      }
+      db.get('user')
+      .push(user)
+      .write();
+      res.redirect('login');
+    }else{
+      error.push("Username exists!");
+      res.render('register',{error});
+    }
+  });
+
+app.get('/login', function(req, res) {
+    let error=[];
+    res.render('login',{error});
+});
+app.post('/login',async function(req, res) {
+    let error=[];
+    let isPasswordMatch=false;
+    let ifus=db.get('user').find({username: req.body["username"]}).value();
+    if(typeof ifus !="undefined"){
+      isPasswordMatch = await bcrypt.compare(req.body["psw"], ifus.password)
+    }
+    let username = req.body.username;
+    if(isPasswordMatch){
+      const token = jwt.sign({user:username},'iframe')
+      res.cookie('authcookie',token,{maxAge:86400000,httpOnly:true}) 
+      res.redirect("manage")
+    }else{
+      error.push("Username or password is incorrect !")
+      res.render("login",{error})
+    }
+  });
+  app.get('/user/:us', function (req, res) {
+    let us=req.params.us;
+    let resu= db.get('user')
+    .remove({username:us})
+    .write()
+    if(resu.length>0){
+      res.json("success")
+    }else{
+      res.json("no success")
+    }
+   });
+  app.all('*', checkToken);
+  app.post('/api/insert',function(req,res){
+    let dt=req.body;
+    dt.param=dt.param.toLowerCase();
+    let ck=getvl(dt.param);
+   if(typeof ck==="undefined"){
+    db.get('posts').push(dt)
+    .write();
+    res.json({"status":"success"})
+   }else{
+    res.json({"status":"duplicate","id":dt.id})
+   }
+   
+  });
+  app.put('/api/update/:id',function(req,res){
+    let prid=req.params.id;
+    let dt=req.body;
+    dt.param=dt.param.toLowerCase();
+    let id={"id":prid};
+    let ck=getvl(dt.param);
+    let ifold=getid(prid);
+    if(typeof ck==="undefined" ||ck.id==prid ){
+      db.get('posts')
+      .find(id)
+      .assign(dt)
+      .write();
+      res.json({"status":"success"})
+    }else{
+      res.json({"status":"duplicate","ifold":ifold})
+    }
+   
+  });
+  app.delete('/api/delete/:id',function(req,res){
+    let idpr=req.params.id;
+    db.get('posts')
+      .remove({id:idpr})
+      .write()
+  res.json({"status":"success"})
+  });
+app.get('/logout', function (req, res) {
+    res.cookie('authcookie',"token",{maxAge:0,httpOnly:true}) 
+    res.redirect("login")
+});
+app.get('/manage' ,function(req, res) {
+    let dtall=db.get('posts').value();
+    let user=req.user;
+    res.render('manage',{dtall,user});
+});
+app.get('*', function(req, res){
+  res.render('404');
+});
+var server = app.listen(PORT, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Ung dung Node.js dang hoat dong tai dia chi: http://%s:%s", host, port)
+  });
+  server.keepAliveTimeout=60000;
